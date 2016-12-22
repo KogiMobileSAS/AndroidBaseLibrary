638936ef864a77f262259b97e9c861c342fb64d9
Pedro Manuel Scott <pedro@kogimobile.com>
2016-12-22 15:04:17 -0500

    Update the Licenses of the base class.

--- baselibrary/src/main/java/com/kogimobile/android/baselibrary/app/base/adapter/BaseSimpleAdapter.java
+++ baselibrary/src/main/java/com/kogimobile/android/baselibrary/app/base/adapter/BaseSimpleAdapter.java
@@ -14,23 +14,22 @@ import java.util.List;
 import butterknife.ButterKnife;
 
 /**
- * @author pedroscott. scott7462@gmail.com
- * @modified Julian Cardona. julian@kogimobile.com
- * @version 9/4/16.
+ * @author Pedro Scott. scott7462@gmail.com
+ * @version 9/4/16. *
+ *          Copyright 2015 Kogi Mobile
  *          <p>
- *          Copyright (C) 2015 The Android Open Source Project
- *          <p/>
  *          Licensed under the Apache License, Version 2.0 (the "License");
  *          you may not use this file except in compliance with the License.
  *          You may obtain a copy of the License at
- *          <p/>
- * @see <a href = "http://www.aprenderaprogramar.com" /> http://www.apache.org/licenses/LICENSE-2.0 </a>
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *          <p>
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *          <p>
+ *          Unless required by applicable law or agreed to in writing, software
+ *          distributed under the License is distributed on an "AS IS" BASIS,
+ *          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *          See the License for the specific language governing permissions and
+ *          limitations under the License.
+ * @modified Julian Cardona. julian@kogimobile.com
  */
 public abstract class BaseSimpleAdapter<T, H extends BaseSimpleAdapter.BaseViewHolder> extends RecyclerView.Adapter<H> {
 
@@ -306,7 +305,7 @@ public abstract class BaseSimpleAdapter<T, H extends BaseSimpleAdapter.BaseViewH
         }
     }
 
-    public abstract static class BaseViewHolder<T> extends RecyclerView.ViewHolder{
+    public abstract static class BaseViewHolder<T> extends RecyclerView.ViewHolder {
 
         public BaseViewHolder(View itemView) {
             super(itemView);
@@ -497,16 +496,16 @@ public abstract class BaseSimpleAdapter<T, H extends BaseSimpleAdapter.BaseViewH
         return selectedItems;
     }
 
-    public boolean ifValidCondition(T t){
+    public boolean ifValidCondition(T t) {
         return true;
     }
 
     @CallSuper
     @Override
     public void onBindViewHolder(H holder, int position) {
-        if(holder instanceof EmptyViewHolder){
+        if (holder instanceof EmptyViewHolder) {
             ((EmptyViewHolder) holder).bindView();
-        }else{
+        } else {
             holder.bindView(items.get(getItemPosition(position)));
         }
     }
