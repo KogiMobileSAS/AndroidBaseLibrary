cbb8246afcab68c3c77830d780f62fb1608e1c6e
Pedro Manuel Scott <pedro@kogimobile.com>
2017-01-04 15:27:01 -0500

    Update the event snack bar to manage resources and Spannables

--- baselibrary/src/main/java/com/kogimobile/android/baselibrary/app/busevents/EventSnackbarMessage.java
+++ baselibrary/src/main/java/com/kogimobile/android/baselibrary/app/busevents/EventSnackbarMessage.java
@@ -1,13 +1,35 @@
 package com.kogimobile.android.baselibrary.app.busevents;
 
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.text.Spannable;
 import android.view.View;
 
+/**
+ * @author Julian Cardona. julian@kogimobile.com
+ * @version 9/4/16. *
+ *          Copyright 2015 Kogi Mobile
+ *          <p>
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
+ *          <p>
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *          <p>
+ *          Unless required by applicable law or agreed to in writing, software
+ *          distributed under the License is distributed on an "AS IS" BASIS,
+ *          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *          See the License for the specific language governing permissions and
+ *          limitations under the License.
+ * @modified Pedro Scott. scott7462@gmail.com
+ */
 public class EventSnackbarMessage {
 
     public static final int NONE_VIEW = -1;
 
     private String message;
+    private int messageResource;
+    private Spannable messageSpannable;
     private String action;
     private View.OnClickListener actionListener;
     private int viewId = NONE_VIEW;
@@ -17,8 +39,35 @@ public class EventSnackbarMessage {
         return message;
     }
 
-    public EventSnackbarMessage withMessage(String message){
+    public int getMessageResource() {
+        return messageResource;
+    }
+
+    public Spannable getMessageSpannable() {
+        return messageSpannable;
+    }
+
+    /**
+     * @param message text to show in the SnackBar
+     */
+    public EventSnackbarMessage withMessage(String message) {
         this.message = message;
+        return this;
+    }
+
+    /**
+     * @param message text to show in the SnackBar
+     */
+    public EventSnackbarMessage withMessage(int message) {
+        this.messageResource = message;
+        return this;
+    }
+
+    /**
+     * @param message text to show in the SnackBar
+     */
+    public EventSnackbarMessage withMessage(Spannable message) {
+        this.messageSpannable = message;
         return this;
     }
 
@@ -40,7 +89,7 @@ public class EventSnackbarMessage {
         return callback;
     }
 
-    public EventSnackbarMessage withCallback(Snackbar.Callback callback){
+    public EventSnackbarMessage withCallback(Snackbar.Callback callback) {
         this.callback = callback;
         return this;
     }
@@ -49,9 +98,28 @@ public class EventSnackbarMessage {
         return viewId;
     }
 
-    public EventSnackbarMessage withViewId(int id){
+    public EventSnackbarMessage withViewId(int id) {
         this.viewId = id;
         return this;
     }
 
+    /**
+     * This method is the delegate to manage the negative button text by difference source of text
+     * like resource, strings or Spannable and set the listener to the button
+     * Note: this methods have priorities in the text 1) resource, 2) String, 3) Spannable.
+     *
+     * @param root is the view where the is showing
+     * @return the SnackBar
+     */
+    public Snackbar managerSnackBar(View root, int lengthLong) {
+        if (getMessageResource() != -1) {
+            return Snackbar.make(root, getMessageResource(), lengthLong);
+        } else if (getMessage() != null) {
+            return Snackbar.make(root, getMessage(), lengthLong);
+        } else if (getMessageSpannable() != null) {
+            return Snackbar.make(root, getMessageSpannable(), lengthLong);
+        } else {
+            return Snackbar.make(root, "", lengthLong);
+        }
+    }
 }
